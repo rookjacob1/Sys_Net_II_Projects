c737d73c36cb2c35f1924381e51f88d73206b84a
rookj <jpr28@students.uwf.edu>
2018-02-11 01:49:50 -0600

    Finished working on validateArgv, may need to add more in the future.

--- Project 1/bbpeer.h
+++ Project 1/bbpeer.h
@@ -15,11 +15,16 @@
 #include"bulletinBoard.h"
 
 /*
- * @breif	validateArgv
+ * @breif	validateArgv	The validateArgv() call checks if the arguments passed to the main
+ * function are in the correct format and have appropriate values.
  *
- * @param	argc
+ * @param	argc			Number of arguments passed to main()
  *
- * @param	argv
+ * @param	argv			Argument vector passed to main()
+ *
+ * @return	int				validateArgv() returns 0 if the arguments indicate a peer joining a
+ * new ring, 1 if the arguments indicate a peer joining an existing ring, and -1 if the arguements
+ * are in the wrong format or have the wrong corresponding values.
  */
 int validateArgv(int argc, char *argv[]);
 
