eb0b2f6ef74d177b5f7b3188fac9ad7c7e230edc
rookj <jpr28@students.uwf.edu>
2018-02-10 23:07:06 -0600

    Added bulletinBoard.h to hold struct for token and associated variables

---
+++ Project 1/bulletinBoard.h
@@ -0,0 +1,21 @@
+/*
+ * @file bbserver.h
+ *
+ * @author Jacob Rook
+ * @date 02/10/2018
+ * @info Course COP 4635
+ * Project 1
+ */
+
+#ifndef BULLETINBOARD_H_
+#define BULLETINBOARD_H_
+
+#define SERVER_PORT 60028
+
+struct token_t {
+
+};
+
+
+
+#endif /* BULLETINBOARD_H_ */
