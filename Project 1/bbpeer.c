40a24c26b40df94eba64086d3a5104caa4d4b886
rookj <jpr28@students.uwf.edu>
2018-02-10 19:51:46 -0600

    Added header comments on files

--- Project 1/bbpeer.c
+++ Project 1/bbpeer.c
@@ -1,8 +1,10 @@
 /*
- * bbpeer.c
+ * @file bbpeer.c
  *
- *  Created on: Feb 10, 2018
- *      Author: rookj
+ * @author Jacob Rook
+ * @date 02/10/2018
+ * @info Course COP 4635
+ * Project 1
  */
 
 #include"headerFiles.h"
--- Project 1/headerFiles.h
+++ Project 1/headerFiles.h
@@ -1,8 +1,10 @@
 /*
- * headerFiles.h
+ * @file headerFiles.h
  *
- *  Created on: Feb 10, 2018
- *      Author: rookj
+ * @author Jacob Rook
+ * @date 02/10/2018
+ * @info Course COP 4635
+ * Project 1
  */
 
 #ifndef HEADERFILES_H_
