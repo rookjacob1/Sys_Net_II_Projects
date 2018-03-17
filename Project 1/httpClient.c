cbbb1f65d2e776b672ffeb50550fa0e945e8436c
rookjacob <jpr28@students.uwf.edu>
2018-03-17 11:07:20 -0500

    fgets(SERVER_NAME, sizeof(SERVER_NAME) - 1 , stdin);

--- Project 1/httpClient.c
+++ Project 1/httpClient.c
@@ -27,7 +27,7 @@ void getAddressFile(void)
 {
 
 	printf("\nPlease enter the server IP address in dotted decimal form:\n");
-	SERVER_NAME = gets(SERVER_NAME);
+	fgets(SERVER_NAME, sizeof(SERVER_NAME) - 1 , stdin);
 	printf("\nPlease enter the file, with file extension, to be requested from server\n");
 	gets(FILE_NAME);
 
