a01a16743d144a265181cc5081fffe079af0408e
rookjacob <jpr28@students.uwf.edu>
2018-04-11 10:45:23 -0500

    SYNTAX: 	(*serverAddr).sin_family = AF_INET; 	(*serverAddr).sin_port = htons(serverPort); 	(*serverAddr).sin_addr.s_addr = htonl(INADDR_ANY);

--- Project 2/bbserver.c
+++ Project 2/bbserver.c
@@ -61,10 +61,10 @@ void validateArgv(int argc, char *argv[], int *serverPort, int *numHosts)
 void createBindSocket(struct sockaddr_in *serverAddr, int *serverPort, int *socketDescriptor)
 {
 	//Build local server socket address
-	memset(serverAddr, 0, sizeof(*serverAddr));
-	*serverAddr.sin_family = AF_INET;
-	*serverAddr.sin_port = htons(serverPort);
-	*serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	memset(serverAddr, 0, sizeof(struct sockaddr_in));
+	(*serverAddr).sin_family = AF_INET;
+	(*serverAddr).sin_port = htons(serverPort);
+	(*serverAddr).sin_addr.s_addr = htonl(INADDR_ANY);
 
 	//Create socket
 	socketDescriptor = socket(PF_INET,SOCK_DGRAM,0);
