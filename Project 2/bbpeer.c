bf846705a4c876b09430bb0a9decd40642bda6ba
rookjacob <jpr28@students.uwf.edu>
2018-04-20 22:50:06 -0500

    PEER: Commented out thre resending of address in initiation

--- Project 2/bbpeer.c
+++ Project 2/bbpeer.c
@@ -316,7 +316,7 @@ void determineInitiator(void)
 				printf("Host port number: %d < received port: %d\n", HOST_PORT, tmpPortNumber);
 				printf("Re-sending host port number %d to next peer with port number %d\n\n", HOST_PORT, NEXT_PEER_PORT);
 
-				sendto(SOCKET_D, &OUT_MESSAGE, sizeof(OUT_MESSAGE), 0, (struct sockaddr *)&NEXT_PEER_ADDR, sizeof(NEXT_PEER_ADDR));
+				//sendto(SOCKET_D, &OUT_MESSAGE, sizeof(OUT_MESSAGE), 0, (struct sockaddr *)&NEXT_PEER_ADDR, sizeof(NEXT_PEER_ADDR));
 			}
 		}
 		else//The initiation process has ended, i.e. the initiation has been found
