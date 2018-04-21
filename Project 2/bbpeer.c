ec539e305f437250ad238c024acdb708550dbd5b
rookjacob <jpr28@students.uwf.edu>
2018-04-20 22:48:08 -0500

    PEER:		if(fseek(fp, (READ_BIT - 1) * MESSAGE_SIZE, SEEK_SET))

--- Project 2/bbpeer.c
+++ Project 2/bbpeer.c
@@ -586,7 +586,7 @@ void bulletinBoardRead(void)
 		if(fp == NULL)
 			error("Error opening file");
 
-		if(fseek(fp, READ_BIT * MESSAGE_SIZE, SEEK_SET))
+		if(fseek(fp, (READ_BIT - 1) * MESSAGE_SIZE, SEEK_SET))
 			error("Error with reading file");
 
 		//Reading from the bulletin board file
